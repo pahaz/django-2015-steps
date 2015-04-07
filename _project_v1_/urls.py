ecc4db3eb77e171756fc10e09ede63be094c5882
pahaz <pahaz.blinov@gmail.com>
2015-04-07 19:25:29 +0500

    init!

---
+++ _project_v1_/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', '_project_v1_.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', index),
+    url(r'^admin/', include(admin.site.urls)),
+]
