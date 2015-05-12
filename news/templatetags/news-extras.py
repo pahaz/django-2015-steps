fe2b20946ae414b15dc4fd7153d9e12ad1fd89c3
pahaz <pahaz.blinov@gmail.com>
2015-05-12 10:03:27 +0500

    style(news.models.News): add missed migration

--- news/templatetags/news-extras.py
+++ news/templatetags/news-extras.py
@@ -7,16 +7,19 @@ from news.models import News
 
 register = Library()
 
+
 @register.filter
 def lower(value):
     return value.lower()
+
 
 @register.simple_tag(takes_context=True)
 def current_time(context, format_string):
     return datetime.datetime.now().strftime(format_string)
 
+
 @register.assignment_tag(takes_context=True)
 def last_news(context, count):
     all_news = News.objects.all()
     counts_news = len(all_news)
-    return all_news[counts_news - count : counts_news]
+    return all_news[counts_news - count: counts_news]
