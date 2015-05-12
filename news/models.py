f511903240401c944b82b85ed71ee8b9d393c969
pahaz <pahaz.blinov@gmail.com>
2015-05-12 19:00:10 +0500

    add abstract Timable model

--- news/models.py
+++ news/models.py
@@ -2,19 +2,25 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Tag(models.Model):
+class Timable(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Tag(Timable, models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 
-class News(models.Model):
+class News(Timable, models.Model):
     title = models.CharField(max_length=200, verbose_name="Название")
     body = models.TextField(verbose_name="Текст")
     author = models.ForeignKey(User, blank=True, null=True)  # TODO: fixit!
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     tags = models.ManyToManyField(Tag, blank=True)
     approved = models.BooleanField(default=False)
 
